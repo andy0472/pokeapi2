d70501260c601b924d3a17971d75862cd5388460
andyr <andyrodriguezjimenez@gmail.com>
2021-10-05 16:35:48 -0400

    Se modifico main adding app file

--- main.py
+++ main.py
@@ -4,12 +4,6 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 from app import app
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     app.run(debug=False,host='0.0.0.0')
