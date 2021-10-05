2de27917cf86946c943dba07496fee6f147aca27
andyr <andyrodriguezjimenez@gmail.com>
2021-10-05 15:56:02 -0400

    Se modifico main adding app file

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from app import app
 
 
 def print_hi(name):
@@ -11,6 +12,6 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    app.run(debug=False,host='0.0.0.0')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
